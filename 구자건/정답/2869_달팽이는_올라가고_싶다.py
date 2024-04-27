4628d85ff733545694b55f3ec27f722659942089
구자건 <wkrjsdl5632@gmail.com>
2024-04-27 10:42:00 +0900

    24-04-27 알고리즘 커밋

--- 구자건/정답/2869_달팽이는_올라가고_싶다.py
+++ 구자건/정답/2869_달팽이는_올라가고_싶다.py
@@ -1,10 +1,2 @@
-from sys import stdin
-
-num1, num2, num3 = map(int, stdin.readline().split(' '))
-
-climbing = num1 - num2
-first_condition = num3 - num1
-
-if first_condition % climbing == 0:
-    print(first_condition // climbing + 1)
-else: print(first_condition // climbing + 2)
+num1, num2, num3 = map(int, input().split(' '))
+print(((num3 - num1) // (num1 - num2)) + 2) if (num3 - num1) % (num1 - num2) else print(((num3 - num1) // (num1 - num2)) + 1)
