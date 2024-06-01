2b2c676b43c1e8b802ce41667820f1335a1e06ee
구자건 <wkrjsdl5632@gmail.com>
2024-06-01 12:52:34 +0900

    24-06-01 알고리즘 커밋

--- 구자건/미완성/25189_시니컬한_개구리.py
+++ 구자건/미완성/25189_시니컬한_개구리.py
@@ -1,5 +1,6 @@
 from sys import stdin
 from collections import deque
+INF = 10 ** 9
 def cynical_frog(start_y, start_x, home_y, home_x):
     dx = [1, -1, 0, 0]
     dy = [0, 0, -1, 1]
@@ -16,35 +17,39 @@ def cynical_frog(start_y, start_x, home_y, home_x):
         for i in range(4):
             x = cur_x + (dx[i]*pond_energy)
             y = cur_y + (dy[i]*pond_energy)
-            
-            if 0 <= x < map_x and 0 <= y < map_y and visited_map[y][x] == False:
+            if 0 <= x < map_x and 0 <= y < map_y and visited_map[y][x] == INF and super_jump_enable == True:
                 if y == home_y and x == home_x:
                     return move + 1
                 bfs_list.append((y, x, move+1, super_jump_enable))
-                visited_map[y][x] = True
-                
+                visited_map[y][x] = move+1
+            if 0 <= x < map_x and 0 <= y < map_y and visited_map[y][x] == INF and super_jump_enable == False:
+                if y == home_y and x == home_x:
+                    return move + 1
+                bfs_list.append((y, x, move+1, super_jump_enable))
+                jump_visited_map[y][x] = move+1
+            
         if super_jump_enable == True:
             for i in range(0, map_x):
-                if jump_visited_map[cur_y][i] == False:
+                if jump_visited_map[cur_y][i] == INF and move + 1 < visited_map[cur_y][x]:
                     if cur_y == home_y and i == home_x:
                         return move + 1
                     bfs_list.append((cur_y, i, move+1, False))
-                    jump_visited_map[cur_y][i] = True
+                    jump_visited_map[cur_y][i] = move+1
                     
             for i in range(0, map_y):
-                if jump_visited_map[i][cur_x] == False:
+                if jump_visited_map[i][cur_x] == INF and move + 1 < visited_map[y][cur_x]:
                     if i == home_y and cur_x == home_x:
                         return move + 1
                     bfs_list.append((i, cur_x, move+1, False))
-                    jump_visited_map[i][cur_x] = True
+                    jump_visited_map[i][cur_x] = move+1
     
     return -1
 
 map_y, map_x = map(int, stdin.readline().split(' '))
 start_y, start_x, home_y, home_x = map(int, stdin.readline().split(' '))
 pond = list()
-visited_map = [[False for _ in range(map_x)]for __ in range(map_y)]
-jump_visited_map = [[False for _ in range(map_x)]for __ in range(map_y)]
+visited_map = [[INF for _ in range(map_x)]for __ in range(map_y)]
+jump_visited_map = [[INF for _ in range(map_x)]for __ in range(map_y)]
 for _ in range(map_y):
     pond.append(list(map(int, stdin.readline().split(' '))))
 
