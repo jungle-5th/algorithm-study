e3188e7250a16f79d69fe83de3da56c5a039eec7
구자건 <wkrjsdl5632@gmail.com>
2024-06-29 11:10:06 +0900

    24-06-29 알고리즘 커밋2

---
+++ 구자건/정답/1874_스택수열.py
@@ -0,0 +1,28 @@
+from sys import stdin
+input = stdin.readline
+def stack(n_numbers):
+    answer = list()
+    for _ in range(n_numbers):
+        answer.append(int(input()))
+    count = 0
+    
+    stack_list = list()
+    output = list()
+    for i in range(1, n_numbers+1):
+        stack_list.append(i)
+        output.append('+')
+        if stack_list[len(stack_list)-1] == answer[count]:
+            while stack_list and stack_list[len(stack_list)-1] == answer[count]:
+                stack_list.pop()
+                output.append('-')
+                count += 1
+    if count != n_numbers:
+        print('No')
+        return
+    for i in output:
+        print(i)
+    return
+
+n_numbers = int(input())
+
+stack(n_numbers)
