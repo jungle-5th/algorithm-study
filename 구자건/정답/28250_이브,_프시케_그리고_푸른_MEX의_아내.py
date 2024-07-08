7abf43ab8a04530d5d15ae77d8bd95a2e851fd98
구자건 <wkrjsdl5632@gmail.com>
2024-07-08 12:48:49 +0900

    24-07-08 알고리즘 커밋

--- 구자건/정답/28250_이브,_프시케_그리고_푸른_MEX의_아내.py
+++ 구자건/정답/28250_이브,_프시케_그리고_푸른_MEX의_아내.py
@@ -5,18 +5,18 @@ def sum_mex(n_number):
     a_list.sort()
     count = 0
     sum_zero = 0
-    sum_one = 0
-    sum_else = 0
-    zero_count = 0
+    one_count = 0
     for i in range(n_number):
         if a_list[i] == 1:
-            sum_zero += 2
-        else: sum_zero += 1
+            one_count += 1
+        if a_list[i] > 1:
+            break
+    sum_zero += one_count*2 + (n_number-one_count)
     for i in range(n_number):
         if a_list[i] == 0:
             sum_zero -= 1
             count += sum_zero
-    
+        if a_list[i] != 0: break
     return count
 
 n_number = int(input())
